6fe6e4de5be8275aa4c0701d66a441cecb4f81d1
gamal barakat <93455616+gamalb@users.noreply.github.com>
2022-09-10 13:42:59 +0200

    Add files via upload
     html css bootstrab js

---
+++ project6.js
@@ -0,0 +1,47 @@
+let span=document.querySelector("span ");
+let myname="GROW Up";
+let index=1;
+function text(){
+span.textContent=myname.slice(0,index);
+index++;
+if(index > myname.length)
+{
+    index=1;
+    
+}
+
+}
+setInterval(function()
+{
+    text(); 
+},700);
+
+
+
+
+//start counter protfolie
+let valuedisplays=document.querySelectorAll(".protfolie-3 h2");
+let interval=5000;
+valuedisplays.forEach((valuedisplays)=>{
+    let startvalue=0;
+    let endvalue=parseInt(valuedisplays.getAttribute("data-val"));
+let duration =Math.floor(interval/endvalue);
+let counter=setInterval(function(){
+    startvalue+=1;
+    valuedisplays.textContent=startvalue;
+    if(startvalue==endvalue)
+    clearInterval(counter);
+},duration)
+
+})
+//end counter protfolie
+
+
+
+
+
+
+
+
+
+
